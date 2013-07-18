3e1b38b2a34bc9955b7c98f79097e4694c5fda9a
tsukasa-oneslocus <oneslocus@gmail.com>
2013-07-18 09:27:56 +0900

    bat修正

--- Sass+CoffeeScript/docs/grunt_files/cmd_bat/grunt_jquerybuild_startup.bat
+++ Sass+CoffeeScript/docs/grunt_files/cmd_bat/grunt_jquerybuild_startup.bat
@@ -3,8 +3,4 @@ cd ..
 
 :: if output error status code of git: 128
 git config --global url."https://".insteadOf git://
-
-git clone git://github.com/jquery/jquery.git
-npm install -g bower
-cd jquery && npm install && bower install
-grunt jquery_startup
+git clone git://github.com/jquery/jquery.git && npm install -g bower && cd jquery && npm install && bower install && grunt jquery_startup
