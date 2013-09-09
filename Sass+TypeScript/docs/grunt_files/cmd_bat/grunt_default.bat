51af534ee153545e2534c5a9e38a1b26fe1168fd
tsukasa-oneslocus <oneslocus@gmail.com>
2013-09-09 11:13:01 +0900

    typescript対応

---
+++ Sass+TypeScript/docs/grunt_files/cmd_bat/grunt_default.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+:: change this directory
+grunt
+:: grunt default command
---
+++ Sass+TypeScript/docs/grunt_files/cmd_bat/grunt_install.bat
@@ -0,0 +1,5 @@
+cd /d %~dp0
+cd ..
+:: change parent directory
+npm install && grunt start
+:: module install & make directory
